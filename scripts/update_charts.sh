7bb23e108a0bda3251967b23d97620aabf21b18f
Brady Zuo <brady@zebrium.com>
2020-09-10 14:48:54 -0700

    Add job config to configmap for custom metrics

--- scripts/update_charts.sh
+++ scripts/update_charts.sh
@@ -1,4 +1,5 @@
 cd ../charts
+version=`cat zstats/Chart.yaml | awk '/version: / { print $2 }'`
 helm package zstats
 cd ..
-helm serve --repo-path ./charts --url  https://github.com/zebrium/ze-stats/releases/download/1.39.0
+helm serve --repo-path ./charts --url  https://github.com/zebrium/ze-stats/releases/download/$version
